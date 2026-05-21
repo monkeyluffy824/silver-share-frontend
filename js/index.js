9dcea57739d6cf0682cacd93d775e50e5fd647b2
monkeyluffy824 <138200438+monkeyluffy824@users.noreply.github.com>
2026-05-21 20:48:32 +0530

    Day-1

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+function theme(color) {
+  ui("theme", color || "#f9bd99");
+}
+
+const mode = () => {
+  let newMode = ui("mode") == "dark" ? "light" : "dark";
+  document.getElementById("modeText").innerHTML  = document.getElementById("modeText").innerHTML  === "<i>light_mode</i>" ? "<i>dark_mode</i>" : "<i>light_mode</i>";
+  ui("mode", newMode);
+}
+
+document.getElementById("modeText").innerHTML  = "<i>light_mode</i>";
+
+window.addEventListener("DOMContentLoaded", () => theme());
