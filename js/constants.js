54cd1af49eddd58463088a0a6269a4f2caf4c168
monkeyluffy824 <138200438+monkeyluffy824@users.noreply.github.com>
2026-05-25 22:00:14 +0530

    u[pload core day-2

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,4 @@
 //This file contains the cconstants which will be used in all the files.
 export const BASE_URI = "https://silver-share.onrender.com";
 export const CHUNK_SIZE = 20*1024*1024;
+export const FILE_TYPE_LIMIT = 25*1024*1024;
--- js/upload.js
+++ js/upload.js
@@ -1,5 +1,5 @@
-import { BASE_URI,CHUNK_SIZE } from "./constants";
-import { calculateCheckSum, encryptedChunk } from "./crypto";
+import { BASE_URI,CHUNK_SIZE, FILE_TYPE_LIMIT } from "./constants";
+import { calculateCheckSum, encryptedChunk, exportKey, generateEncryptionKey } from "./crypto";
 const MAX_FILE_SIZE =200*1024*1024;
 const MAX_TOTAL_FILES = 5;
 
@@ -9,7 +9,12 @@ const state ={
     cryptoKey:"",
     base64Key:"",
     isUploading: false,
-    shortCode:""
+    shortCode:"",
+    title:"",
+    message:"",
+    expiryCount:"",
+    isComplete: false,
+    filePreSignedUrls:{}
 }
 
 
@@ -82,3 +87,98 @@ function createTransferRequestBody(files,title,message,expiryCount){
     return transJson;
 }
 
+
+async function mainUploadFlow(){
+    if(state.files.length === 0){
+        throw Error("Please Select atlest 1 file");
+    }
+
+    if(state.isUploading){
+        return;
+    }
+
+    state.isUploading = true;
+
+    try{
+        
+        await initiateTransfer();
+        state.cryptoKey = await generateEncryptionKey();
+        state.base64Key = await exportKey(state.cryptoKey);
+        const filesJson ={};
+        for(let i=0; i< state.files.length;i++){
+            const file = state.files[i];
+            const iniFileRequestBody = createFileRequestBody(file);
+            const etgs =[];
+            if(state.filePreSignedUrls[Object.keys()[i]].length === 1 && file.size < FILE_TYPE_LIMIT){
+                etgs = await uploadSingleFile(file,state.cryptoKey,state.filePreSignedUrls[Object.keys()[i]][0]);
+            }else{
+                etgs = await multipartUpload(file,state.cryptoKey,state.filePreSignedUrls[Object.keys()[i]]);
+            }
+            completeFileBody(etgs,Object.keys()[i],filesJson);
+        }
+        const comJson = completeTransferBody(filesJson);
+        await completeTransfer(comJson);
+
+        state.isUploading = false;
+        state.isComplete = true;
+    }catch(error){
+        console.log(error);
+        clearoutState();
+    }
+}
+
+async function initiateTransfer(){
+    try{
+        const transferJson = createTransferRequestBody(state.files,state.title,state.message,state.expiryCount);
+        const response = await fetch(`${BASE_URI}/transfers/create`,{method: "POST", body : transferJson});
+        if(response.ok){
+            const transferData = await response.json();
+            state.shortCode = transferData["short_code"];
+            state.filePreSignedUrls = transferData["response_files"];
+        }else{
+            throw Error("Error ocurred in creating Transfer");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function completeTransfer(comJson){
+    try{
+        const response = await fetch(`${BASE_URI}/transfers/${state.shortCode}/complete`,{method: "POST", body : comJson});
+        if(response.ok){
+            return;
+        }else{
+            throw Error("Error Occured in completing Transfer");
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+function completeTransferBody(filesJson){
+    const comJson ={};
+    comJson["files"]= filesJson;
+
+    return comJson
+}
+
+function completeFileBody(etgs,fileId,filesJson){
+    const parts ={};
+    parts["file_parts"] = etgs;
+    filesJson[fileId] = parts;
+}
+function clearoutState(){
+    state ={
+                files:[],
+                cryptoKey:"",
+                base64Key:"",
+                isUploading: false,
+                shortCode:"",
+                title:"",
+                message:"",
+                expiryCount:"",
+                isComplete: false,
+                filePreSignedUrls:{}
+            }   
+}
